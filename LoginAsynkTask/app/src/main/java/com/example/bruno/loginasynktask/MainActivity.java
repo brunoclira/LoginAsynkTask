ed7a319e6a5cdc1a1066c368789f07ff00f0ca20
Bruno Lira <brunolyr4@gmail.com>
2015-12-23 18:16:38 -0200

    update

--- LoginAsynkTask/app/src/main/java/com/example/bruno/loginasynktask/MainActivity.java
+++ LoginAsynkTask/app/src/main/java/com/example/bruno/loginasynktask/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.bruno.loginasynktask;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import br.edu.ifpb.notificationwear.asynctask.LoginAsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button asyncTaskButton = (Button) findViewById(R.id.enviarButton);
+
+        asyncTaskButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                EditText nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+                    String nome = nomeEditText.getText().toString();
+                EditText senhaEditText = (EditText) findViewById(R.id.senhaEditText);
+                    String senha = senhaEditText.getText().toString();
+
+                LoginAsyncTask loginAsyncTask = new LoginAsyncTask(view.getContext());
             }
+
         });
     }
 
